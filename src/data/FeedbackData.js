a215328274c98a9a5128e0c4a51e4a4efe40fab5
Harshith <harshith.smartail@gmail.com>
2024-05-29 14:39:21 +0530

    changes_Feedback_app

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'HI tHANK YOU FOR THE wONDDERFUL PRODUCT.',
+  },
+  {
+    id: 2,
+    rating: 1,
+    text: 'HI tHANK YOU FOR THE wONDDERFUL PRODUCT',
+  },
+  {
+    id: 3,
+    rating: 3,
+    text: 'HI tHANK YOU FOR THE wONDDERFUL PRODUCT',
+  },
+]
+
+export default FeedbackData
